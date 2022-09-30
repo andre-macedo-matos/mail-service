ac1d18fdbbb879078f1c57df3103bf9d0cf96ee7
andre-macedo-matos <andremacedomatos@gmail.com>
2022-09-30 03:06:06 -0300

    Add exception Handling

--- src/main/java/org/service/mail/controllers/MailController.java
+++ src/main/java/org/service/mail/controllers/MailController.java
@@ -1,15 +1,12 @@
 package org.service.mail.controllers;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class MailController {
@@ -21,16 +18,19 @@ public class MailController {
 	private SimpleMailMessage message;
 	
 	@PostMapping(value = "/send")
-	public ModelAndView send(HttpServletRequest request,
-							 @RequestParam(value = "name") String name, 
-							 @RequestParam(value = "mail") String mail, 
-							 @RequestParam(value = "msg") String msg) {
+	public String send(@RequestParam(value = "name") String name,
+					   @RequestParam(value = "mail") String mail, 
+					   @RequestParam(value = "msg") String msg) {
 		
 		message.setTo(mail);
 		message.setText(String.format(message.getText(), name, msg));
 		
-		mailSender.send(message);
+		try {
+			mailSender.send(message);
+		} catch (MailException e) {
+			return "Ops, algo deu errado!\n Tente enviar a mensagem em outro momento.";
+		}
 		
-		return new ModelAndView("redirect:" + request.getHeader(HttpHeaders.ORIGIN));
+		return "Email enviado com sucesso";
 	}
 }
