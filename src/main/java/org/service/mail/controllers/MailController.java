237c6467562d1404b10b8a6b7254c82ee854f903
andre-macedo-matos <andremacedomatos@gmail.com>
2022-09-30 00:23:45 -0300

    Add redirect to origin page

--- src/main/java/org/service/mail/controllers/MailController.java
+++ src/main/java/org/service/mail/controllers/MailController.java
@@ -1,11 +1,15 @@
 package org.service.mail.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class MailController {
@@ -17,15 +21,16 @@ public class MailController {
 	private SimpleMailMessage message;
 	
 	@PostMapping(value = "/send")
-	public String send(@RequestParam(value = "name") String name, 
-			           @RequestParam(value = "mail") String mail, 
-			           @RequestParam(value = "msg") String msg) {
+	public ModelAndView send(HttpServletRequest request,
+							 @RequestParam(value = "name") String name, 
+							 @RequestParam(value = "mail") String mail, 
+							 @RequestParam(value = "msg") String msg) {
 		
 		message.setTo(mail);
 		message.setText(String.format(message.getText(), name, msg));
 		
 		mailSender.send(message);
 		
-		return "Email enviado com sucesso!";
+		return new ModelAndView("redirect:" + request.getHeader(HttpHeaders.ORIGIN));
 	}
 }
